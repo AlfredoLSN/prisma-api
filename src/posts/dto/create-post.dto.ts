1517464f6328b9ae9bdb3d153f6c66686ee7ac36
Alfredo Lucas <alfredoliu14@live.com>
2024-08-23 16:12:58 -0300

    finaliza relacionamentos

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreatePostDto {
   @IsString()
@@ -9,6 +9,6 @@ export class CreatePostDto {
   @IsOptional()
   content: string;
 
-  @IsNumber()
-  authorId: number;
+  @IsEmail()
+  authorEmail: string;
 }
--- src/posts/repositories/posts.repository.ts
+++ src/posts/repositories/posts.repository.ts
@@ -3,19 +3,48 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { PostEntity } from '../entities/post.entity';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { Prisma } from '@prisma/client';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 
 @Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    const { authorEmail } = createPostDto;
+
+    delete createPostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundError('Author not found');
+    }
+
+    const data: Prisma.PostCreateInput = {
+      ...createPostDto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
     return this.prisma.post.create({
       data: createPostDto,
     });
   }
 
   async findAll(): Promise<PostEntity[]> {
-    return this.prisma.post.findMany();
+    return this.prisma.post.findMany({
+      include: {
+        author: true,
+      },
+    });
   }
 
   async findOne(id: number): Promise<PostEntity> {
@@ -23,15 +52,56 @@ export class PostsRepository {
       where: {
         id,
       },
+      include: {
+        author: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
     });
   }
 
   async update(id: number, updatePostDto: UpdatePostDto): Promise<PostEntity> {
-    return this.prisma.post.update({
+    const { authorEmail } = updatePostDto;
+    if (!authorEmail) {
+      return this.prisma.post.update({
+        data: updatePostDto,
+        where: { id },
+      });
+    }
+
+    delete updatePostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
       where: {
-        id,
+        email: authorEmail,
       },
-      data: updatePostDto,
+    });
+
+    if (!user) {
+      throw new NotFoundError('Author not found.');
+    }
+
+    const data: Prisma.PostUpdateInput = {
+      ...updatePostDto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+    return this.prisma.post.update({
+      where: { id },
+      data,
+      include: {
+        author: {
+          select: {
+            name: true
+          }
+        }
+      }
     });
   }
 
